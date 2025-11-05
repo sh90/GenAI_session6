2999176adacfb2ca271e24f1467b6ffd7759eb26
sh90 <shikhatyagi1990@gmail.com>
2025-11-05 21:49:45 +0530

    Delete examples directory

--- examples/data/tool_registry.json.py
+++
@@ -1,8 +0,0 @@
-{
-  "tools": [
-    {"name": "get_customer", "input_schema": {"customer_id": "string"}},
-    {"name": "get_tickets", "input_schema": {"customer_id": "string"}},
-    {"name": "get_weather", "input_schema": {"city": "string"}},
-    {"name": "rag_support", "input_schema": {"query": "string"}}
-  ]
-}
